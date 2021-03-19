182c8262daf2af3a635537c12d2a7e86b50f721d
yhan0704 <hanyc1205@gmail.com>
2021-03-18 21:56:32 -0400

    add testNG/; git comm a q quit exit q

---
+++ src/main/java/maven3/TestNG.java
@@ -0,0 +1,28 @@
+package maven3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNG {
+	WebDriver driver;
+	String url = "https://www.google.com/";
+	@BeforeClass
+	public void before() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		Thread.sleep(3000);
+		System.out.println("aweffafe");
+	}
+	@Test
+	public void test() {
+		System.out.println("afeawf");
+	}
+	@AfterClass
+	public void after() {
+		driver.close();
+	}
+}
